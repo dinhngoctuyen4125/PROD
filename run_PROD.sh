931a48cb513654afce18da3910d2c43cc00ad41c
dinhngoctuyen4125 <tuyenotk4125@gmail.com>
2026-08-18 14:41:10 +0700

    fddF

---
+++ run_PROD.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Running script for the copyrighted code unlearning task
+
+lr=5e-6
+
+Model="codellama/CodeLlama-7b-hf"
+ModelName="CodeLlama-7b-hf"
+ModelPath="learned_model"
+DatasetPath="data/codellama/depAPI.json"
+SaveModelPath="outputs/models/PROD_lr${lr}"
+
+python PROD.py \
+--model_name ${Model} \
+--model_path ${ModelPath} \
+--output_dir ${SaveModelPath} \
+--train_data_path ${DatasetPath} \
+--alpha 0.0 \
+--num_train_epochs 10 \
+--learning_rate ${lr} \
+--per_device_train_batch_size 1 \
+--gradient_accumulation_steps 32 \
+--logging_steps 1 \
+--save_total_limit 2 \
+--overwrite_output_dir \
+--do_train \
+--save_strategy no
