edaf8c45d64fd987297847461a82fde69025acf6
dinhngoctuyen4125 <tuyenotk4125@gmail.com>
2026-08-19 03:45:40 +0700

    Fdfd

--- run_PROD.sh
+++ run_PROD.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# Running script for the copyrighted code unlearning task
+#SBATCH --job-name=prod
+#SBATCH --output=logs/output_%j.log
+#SBATCH --error=logs/error_%j.log
+#SBATCH --partition=defq
+#SBATCH --qos=short
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128G
 
 lr=5e-6
 
