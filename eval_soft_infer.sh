d49ac3fa577e87e76a8e817a0ad209b7e83581fc
dinhngoctuyen4125 <tuyenotk4125@gmail>
2026-08-25 11:22:24 +0700

    lmao

--- eval_soft_infer.sh
+++ eval_soft_infer.sh
@@ -11,19 +11,26 @@
 #SBATCH --mem=128G
 
 BASE_MODEL="codellama/CodeLlama-7b-hf"
-PROD_MODEL="outputs/models/PROD_lr5e-6/PROD_epoch1_lr5e-06"
+TESTPATH="../Data-Collection/codellama/D_test.json"
 
-for SEED in 0
+for EPOCH in 0 1 2 3 4
 do
-    # Eval on D_test.json
-    TESTPATH="../Data-Collection/codellama/D_test.json"
-    python eval_o3.py \
-        --test_dataset ${TESTPATH} \
-        --base_model ${BASE_MODEL} \
-        --prod_model ${PROD_MODEL} \
-        --seed ${SEED} \
-        --ood_type "_all" \
-        --ood_weights "./ood_checkpoints_codellama_${SEED}/" \
-        --ood_base_model "microsoft/codebert-base" \
-        --ood_setting_name "codellama"
+    PROD_MODEL="outputs/models/PROD_lr5e-6/PROD_epoch${EPOCH}_lr5e-06"
+    echo ""
+    echo "============================================"
+    echo "  Running epoch ${EPOCH}: ${PROD_MODEL}"
+    echo "============================================"
+
+    for SEED in 0
+    do
+        python eval_o3.py \
+            --test_dataset ${TESTPATH} \
+            --base_model ${BASE_MODEL} \
+            --prod_model ${PROD_MODEL} \
+            --seed ${SEED} \
+            --ood_type "_all" \
+            --ood_weights "./ood_checkpoints_codellama_${SEED}/" \
+            --ood_base_model "microsoft/codebert-base" \
+            --ood_setting_name "codellama"
+    done
 done
